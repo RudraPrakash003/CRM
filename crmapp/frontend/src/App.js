f627eeac844fff05e62da027db1fff238302b0b2
Yaralagadda Anuhya <137102795+Anuhya30110@users.noreply.github.com>
2024-05-15 15:11:59 +0530

    Update App.js

--- crmapp/frontend/src/App.js
+++ crmapp/frontend/src/App.js
@@ -7,19 +7,32 @@ import  './App.css';
 
 const BaseLayout = () => (
   <div className="container-fluid">
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">CRM</a>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
+  <a className="navbar-brand" href="#">CRM</a>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
       <a className="nav-item nav-link" href="/">CUSTOMERS</a>
       <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
-      
+    </div>
+    <div className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="/dashboard">Dashboard</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/customers">Customers</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/products">Products</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" onClick={() => { localStorage.removeItem('token'); window.location.href = '/'; }}>Logout</a>
+      </li>
     </div>
   </div>
-</nav>  
+</nav>
 
     <div className="content">
       <Routes>
